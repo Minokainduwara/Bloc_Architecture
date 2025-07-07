80e04f938e541ea9aaeead4f6141dc28558070aa
Minokainduwara <minokainduwara@gmail.com>
2025-07-08 00:33:16 +0530

    cubit

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+class CounterCubit extends Cubit<int>{
+  //pass initial state, you can pass any data type you want
+  CounterCubit() : super(0);
+  
+
+}
